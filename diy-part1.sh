25f37173f76f97743cb36921e265b89e227c3e76
jimway71 <52953096+jimway71@users.noreply.github.com>
2022-06-22 01:05:09 +0800

    Update diy-part1.sh
    try:
    only change diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -14,9 +14,9 @@
 # src-git helloworld https://github.com/fw876/helloworld
 # src-git kenzo https://github.com/kenzok8/openwrt-packages
 # src-git small https://github.com/kenzok8/small
-sed -i '$a src-git small https://github.com/kenzok8/small' feeds.conf.default
-sed -i '$a src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
-sed -i 'src-git helloworld https://github.com/fw876/helloworld' feeds.conf.default
+# X sed -i '$a src-git small https://github.com/kenzok8/small' feeds.conf.default
+# X sed -i '$a src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
+# X sed -i 'src-git helloworld https://github.com/fw876/helloworld' feeds.conf.default
 # sed -i '$a src-git openclash https://github.com/vernesong/OpenClash' feeds.conf.default
 # sed -i '$a src-git cups https://github.com/TheMMcOfficial/lede-cups.git' feeds.conf.default
 # X sed -i '$a src-git cups https://github.com/Gr4ffy/lede-cups.git' feeds.conf.default
@@ -24,9 +24,9 @@ sed -i 'src-git helloworld https://github.com/fw876/helloworld' feeds.conf.defau
 # Add luci-theme-argon
 # rm -rf lede/package/lean/luci-theme-argon
 # git clone https://github.com/jerrykuku/luci-theme-argon.git
-rm -rf package/lean/luci-theme-argon/
+# X rm -rf package/lean/luci-theme-argon/
 # git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon package/luci-theme-argon
-git clone -b master https://github.com/jerrykuku/luci-app-argon-config package/luci-app-argon-config
+# X git clone -b master https://github.com/jerrykuku/luci-app-argon-config package/luci-app-argon-config
 
 #添加额外软件包
 # git clone https://github.com/kongfl888/luci-app-adguardhome.git package/luci-app-adguardhome
@@ -42,3 +42,5 @@ git clone -b master https://github.com/jerrykuku/luci-app-argon-config package/l
 # svn co https://github.com/fw876/helloworld/trunk/v2ray-plugin package/v2ray-plugin
 # svn co https://github.com/fw876/helloworld/trunk/v2ray-core package/v2ray-core
 # svn co https://github.com/fw876/helloworld/trunk/luci-app-ssr-plus package/luci-app-ssr-plus
+
+echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
