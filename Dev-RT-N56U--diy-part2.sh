966251c7f213bd93887862653db77fd0243aee77
jimway71 <52953096+jimway71@users.noreply.github.com>
2022-07-02 20:35:30 +0800

    Create Dev-RT-N56U--diy-part2.sh
    New DIY shell2 for ASUS RT-N56U

---
+++ Dev-RT-N56U--diy-part2.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+#
+# Copyright (c) 2019-2020 P3TERX <https://p3terx.com>
+#
+# This is free software, licensed under the MIT License.
+# See /LICENSE for more information.
+#
+# https://github.com/P3TERX/Actions-OpenWrt
+# File name: Dev-RT-N56U--diy-part2.sh
+# Description: OpenWrt DIY script part 2 (After Update feeds)
+#
+
+# Modify default IP
+# X sed -i 's/192.168.1.1/192.168.2.1/g' package/base-files/files/bin/config_generate
+# 版本号里显示一个自己的名字
+# sed -i "s/OpenWrt/Jimway build $(TZ=UTC-8 date "+%y.%m.%d") @/g" package/lean/default-settings/files/zzz-default-settings
+# 设置密码为空（安装固件时无需密码登陆，然后自己修改想要的密码）
+# sed -i 's@.*CYXluq4wUazHjmCDBCqXF*@#&@g' package/lean/default-settings/files/zzz-default-settings
+# 修改 argon 为默认主题,不再集成luci-theme-bootstrap主题
+#sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci/Makefile
+# 获取luci-app-openclash 编译po2lmo
+# git clone -b master https://github.com/vernesong/OpenClash package/diy-packages/openclash
+# pushd package/diy-packages/openclash/luci-app-openclash/tools/po2lmo
+# make && sudo make install
+# popd
+
+
+# From https://github.com/P3TERX/OpenWrt-Newifi_D2:
+# Modify default IP
+# sed -i 's/192.168.1.1/192.168.1.1/g' package/base-files/files/bin/config_generate
+# Modify hostname
+sed -i 's/OpenWrt/RT-N56UOP/g' package/base-files/files/bin/config_generate
+# Modify the version number
+sed -i "s/OpenWrt /Jimway build $(TZ=UTC-8 date "+%Y.%m.%d") 4 ASUS RT-N56U @ OpenWrt /g" package/lean/default-settings/files/zzz-default-settings
+# Modify default theme
+sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci/Makefile
